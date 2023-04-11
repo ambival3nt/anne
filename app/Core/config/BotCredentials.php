39b4c2573d362c044f27049655977c1ce2d9201b
ambival3nt <64205154+ambival3nt@users.noreply.github.com>
2023-04-11 11:43:34 -0400

    oops lol
    Signed-off-by: ambival3nt <64205154+ambival3nt@users.noreply.github.com>

--- app/Core/config/BotCredentials.php
+++ app/Core/config/BotCredentials.php
@@ -5,21 +5,6 @@ namespace App\Core\config;
 class BotCredentials
 {
     private $token;
-
-    //TODO: MAKE AN ENV FILE goddammit lets just start over with laravel
-
-//    public function setToken(){
-////        $this->token = $_ENV('DISCORD_TOKEN');
-//        $this->token = "MTAzNjI1NjI1MTQxMzI2NjUyMg.G4InvY.SLX82VfTB_bXTj4hpAVlUjA9zfN00lIdgSiK0g";
-//    }
-//
-//    public function getToken($auth = true){
-//        if($auth) {
-////            return $this->token;
-//            return "MTAzNjI1NjI1MTQxMzI2NjUyMg.G4InvY.SLX82VfTB_bXTj4hpAVlUjA9zfN00lIdgSiK0g";
-//        }
-//        return "Unauthorized.";
-//    }
     public function getToken()
     {
         $this->token=getenv('DISCORD_BOT_TOKEN');
