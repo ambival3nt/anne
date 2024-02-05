b7d251013e715309ae4da6ebcd6ba8603364454d
ambival3nt <64205154+ambival3nt@users.noreply.github.com>
2024-02-05 02:43:28 -0500

    Log Paginator
    Signed-off-by: ambival3nt <64205154+ambival3nt@users.noreply.github.com>

--- resources/views/livewire/show-logs.blade.php
+++ resources/views/livewire/show-logs.blade.php
@@ -3,7 +3,9 @@
         <div class="p-fluid-s text-lg text-left font-semibold">Level</div>
         <div class="p-fluid-s text-lg text-left font-semibold">Message</div>
 
-        @foreach ($logData as $log)
+
+
+        @foreach (json_decode($logData)->data as $log)
         <div class="p-fluid-xs [&:nth-child(6n+1)]:bg-midnight-800">
             {{ $log->logged_at }}
         </div>
@@ -16,4 +18,30 @@
             {{ $log->message }}
         </div>
         @endforeach
+
+
+    </div>
+    <div class="join mt-fluid-m place-self-center">
+        <button type="button"
+                class="join-item btn btn-outline btn-primary border-ltblue-55 text-ltblue-55 fill-ltblue-55"
+                wire:click="pageHandler('1')">«
+        </button>
+        <button type="button"
+                class="join-item btn btn-outline btn-primary border-ltblue-55 text-ltblue-55 fill-ltblue-55"
+                wire:click="pageHandler('{{json_decode($logData)->current_page - 1 }}')">«
+        </button>
+
+        <button type="button"
+                class="join-item btn btn-outline btn-primary border-ltblue-55 text-ltblue-55 fill-ltblue-55">
+            Page {{json_decode($logData)->current_page}} / {{json_decode($logData)->last_page}}</button>
+        <button type="button"
+                class="join-item btn btn-outline btn-primary border-ltblue-55 text-ltblue-55 fill-ltblue-55"
+                wire:click="pageHandler('{{json_decode($logData)->current_page + 1}}')">»
+        </button>
+        <button type="button"
+                class="join-item btn btn-outline btn-primary border-ltblue-55 text-ltblue-55 fill-ltblue-55"
+                wire:click="pageHandler('{{json_decode($logData)->last_page}}')">»
+        </button>
+
+
     </div>
